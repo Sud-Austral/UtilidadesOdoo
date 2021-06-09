fa856bb8e1c63f9b6fa4621cf2d757fbd75c3cb9
lmonsalve22 <lmonsalve22@gmail.com>
2021-06-09 12:00:04 -0400

    violencia

---
+++ AplicacionBlanco/AplicacionBlanco/Controllers/MostradorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBlanco.Controllers
+{
+    public class MostradorController : Controller
+    {
+        // GET: Mostrador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Violencia_Genero()
+        {
+            return View();
+        }
+    }
+}
